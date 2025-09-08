099d4646744eee9070a2a591db3245b43125e1d5
ParasharDeb <parashardeb11@gmail.com>
2025-09-08 19:41:57 +0530

    not the best way need to fix the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,20 @@
-export default function Dashboard(){
-    return(
-        <div>
-            hi there
+'use client'
+import LeftNavigation from "../maincomps/Leftnav";
+import CampDashboard from "../maincomps/dashboard-campaigns";
+import LinkedinAccs from "../maincomps/dashboard-linkedin";
+import RecentDashboard from "../maincomps/dashboard-recents";
+
+export default function Dashboard() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex">
+      <LeftNavigation />
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="w-2/3 flex flex-col gap-6">
+          <CampDashboard />
+          <LinkedinAccs />
         </div>
-    )
+        <RecentDashboard />
+      </main>
+    </div>
+  );
 }
